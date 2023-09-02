a24c900f5e95616fd519f9438e7f1fa16cefab41
Kayky Rodrigues <109947756+fs-kayky@users.noreply.github.com>
2023-09-02 03:08:42 -0300

    atualização de incompatibilidade com nomes.

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,10 +1,10 @@
-public class User {
+public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
 
         smartTv.Exibir();
         smartTv.Ligar();
         smartTv.Exibir();
-
+        
     }
 }
